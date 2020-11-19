57a9a121ff0d622542abcce590f52e49c6932346
MairKeen <69979556+MairKeen@users.noreply.github.com>
2020-11-19 14:50:33 -0500

    updated

--- pythonscript.py
+++ pythonscript.py
@@ -1,12 +1,63 @@
-import speech_recognition as sr     # import the library
- 
-r = sr.Recognizer()                 # initialize recognizer
-with sr.Microphone() as source:     # mention source it will be either Microphone or audio files.
-    print("Speak Anything :")
-    audio = r.listen(source)        # listen to the source
-    try:
-        text = r.recognize_google(audio)    # use recognizer to convert our audio into text part.
-        print("You said : {}".format(text))
-    except:
-        print("Sorry could not recognize your voice")    # In case of voice not recognized  clearly
- 
+import os.path
+from os import path 
+import speech_recognition as sr
+import pyttsx3
+from multiprocessing import Process
+import datetime
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour=datetime.datetime.now().hour
+    if hour>=0 and hour<12:
+        speak("Hello,Good Morning")
+        print("Hello,Good Morning")
+    elif hour>=12 and hour<18:
+        speak("Hello,Good Afternoon")
+        print("Hello,Good Afternoon")
+    else:
+        speak("Hello,Good Evening")
+        print("Hello,Good Evening")
+
+
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio=r.listen(source)
+
+        try:
+            statement=r.recognize_google(audio,language='en-in')
+            print(f"user said:{statement}\n")
+
+        except Exception as e:
+            speak("Pardon me, please say that again")
+            return "None"
+        return statement
+
+print("Loading your AI personal assistant G-One")
+speak("Loading your AI personal assistant G-One")
+wishMe()
+
+
+if __name__=='__main__':
+
+
+    while True:
+        speak("Tell me how can I help you now?")
+        statement = takeCommand().lower()
+        if statement==0:
+            continue
+
+if "good bye" in statement or "ok bye" in statement or "stop" in statement:
+            speak('your personal assistant G-one is shutting down,Good bye')
+            print('your personal assistant G-one is shutting down,Good bye')
+break
