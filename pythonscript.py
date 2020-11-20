84411a0275aeb79ea5cf98d65e8055f3fe74d20a
MairKeen <69979556+MairKeen@users.noreply.github.com>
2020-11-19 23:06:09 -0500

    Update pythonscript.py

--- pythonscript.py
+++ pythonscript.py
@@ -7,13 +7,12 @@ import datetime
 
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
-engine.setProperty('voice','voices[0].id')
+engine.setProperty('voice','voices[1].id')
 
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
-
 
 def wishMe():
     hour=datetime.datetime.now().hour
@@ -43,8 +42,8 @@ def takeCommand():
             return "None"
         return statement
 
-print("Loading your AI personal assistant G-One")
-speak("Loading your AI personal assistant G-One")
+print("Loading your AI personal assistant Friday")
+speak("Loading your AI personal assistant Friday")
 wishMe()
 
 
@@ -54,10 +53,10 @@ if __name__=='__main__':
     while True:
         speak("Tell me how can I help you now?")
         statement = takeCommand().lower()
-        if statement==0:
+        if statement == 0:
             continue
 
-if "good bye" in statement or "ok bye" in statement or "stop" in statement:
-            speak('your personal assistant G-one is shutting down,Good bye')
-            print('your personal assistant G-one is shutting down,Good bye')
-break
+        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
+            speak('your personal assistant Friday is shutting down,Good bye')
+            print('your personal assistant Friday is shutting down,Good bye')
+        break
