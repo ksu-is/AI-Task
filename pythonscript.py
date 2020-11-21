5f59dbbf392f35fc6f6fe20d5c19d8143ab1b28f
MairKeen <69979556+MairKeen@users.noreply.github.com>
2020-11-21 14:38:07 -0500

    Put all the print statements in text to speech
    with pyttsx3 users can print their statement in text to speech form so the once the AI is activated it will tell them their upcoming assigments.

--- pythonscript.py
+++ pythonscript.py
@@ -6,12 +6,13 @@ import time
 import subprocess
 import requests
 
+
+Name="Friday"
+
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
 engine.setProperty('voice','voices[1].id')
 
-Name="Friday"
-speak = "Friday here"
 
 def speak(text):
     engine.say(text)
@@ -19,33 +20,29 @@ def speak(text):
 
 def tellme():
     hour=datetime.datetime.now().hour
-    if hour>=0 and hour<12:
-        speak("Hello,")
-        print("Hello,Good Morning")
-    elif hour>=12 and hour<18:
-        speak("Hello,Good Afternoon")
-        print("Hello,Good Afternoon")
+    if hour>=0 and hour<10:
+        speak("Good morining, you have Marketing Paper due at 11:59 PM")
+    elif hour>=10 and hour<1:
+        speak("Good Afternoon, you have python 2 module 2 assigment due tomorrow at 11:30 PM")
     else:
-        speak("Hello,Good Evening")
-        print("Hello,Good Evening")
+        speak("Hello, you have no assigments due today")
 
-def takeCommand():
+def Command():
     r=sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
         audio=r.listen(source)
 
-        try: 
-            statement=r.recognize_google(audio,language='en-in')
-            print(f"user said:{statement}\n")
+    try: 
+        statement=r.recognize_google(audio,language='en-in')
+        print(f"user said:{statement}\n")
 
-        except Exception as e:
-            speak("Pardon me, please say that again")
-            return "None"
+    except:
+        speak("Pardon me, I didn't quite catch that")
+        return "None"
 
-        return statement
+    return statement
 
-print("Loading your AI personal assistant Friday")
 speak("Loading your AI personal assistant Friday")
 tellme()
 
@@ -54,11 +51,10 @@ if __name__=='__main__':
 
     while True:
         speak("Tell me how can I help you now?")
-        statement = takeCommand().lower()
+        statement = Command().lower()
         if statement == 0:
             continue
 
         if "good bye" in statement or "ok bye" in statement or "stop" in statement:
             speak('your personal assistant Friday is shutting down,Good bye')
-            print('your personal assistant Friday is shutting down,Good bye')
         break
